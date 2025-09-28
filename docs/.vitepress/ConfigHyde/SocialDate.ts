5878607120d7489b842edf9325a878bc2cfdade1
work-pc <2675263825@qq.com>
2025-09-28 14:20:06 +0800

    修复首页侧边栏 个人联系方式 图标无法对齐 问题

--- docs/.vitepress/ConfigHyde/SocialDate.ts
+++ docs/.vitepress/ConfigHyde/SocialDate.ts
@@ -3,12 +3,14 @@ import { Social } from "vitepress-theme-teek/config";
 
 export const SocialDate: Social[] = [
   {
-    icon: "mdi:github",
+    icon: "icon-github",
+    iconType: "iconfont",
     name: "GitHub",
-    link: "https://cnb.cool/onedayxyy",
+    link: "https://cnb.cool/onedayxyy/vitepress-theme-teek-one-public",
   },
   {
-    icon: "simple-icons:gitee",
+    icon: "icon-gitee2",
+    iconType: "iconfont",
     name: "Gitee",
     link: "https://cnb.cool/onedayxyy/vitepress-theme-teek-one-public",
   },
@@ -16,14 +18,12 @@ export const SocialDate: Social[] = [
     icon: "icon-qq",
     iconType: "iconfont",
     name: "QQ",
-    // link: "http://wpa.qq.com/msgrd?v=3&uin=2675263825&site=qq&menu=yes",
-    link: "https://img.onedayxyy.cn/images/Teekwebsite/teek-hg-qq.jpg",
+    link: "http://wpa.qq.com/msgrd?v=3&uin=2675263825&site=qq&menu=yes",
   },
   {
     icon: "icon-mobile",
     iconType: "iconfont",
     name: "微信",
-    // link: "https://onedayxyy.cn/?contact=true",
     link: "https://img.onedayxyy.cn/images/image-20230107215114763-1694437284994-1-1697348761221-1-1697407921190-1-1697636582091-3-1698965093137-1.png",
   },
 ];
